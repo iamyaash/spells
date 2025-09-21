01ea365bf4d8d926841eb6eb3397fa1a4cb20f2f
iamyaash <iamyaash@outlook.com>
2025-09-21 14:05:46 +0530

    chore: minor changes
    RSS icon added in homepage
    Emoji added for both search & tags
    
    Signed-off-by: iamyaash <iamyaash@outlook.com>

--- scripts/after-clone.sh
+++ scripts/after-clone.sh
@@ -2,14 +2,15 @@
 
 echo "Downloading PaperMod themes to the themes/ directory"
 # Remove existing submodule files and Git data
-rm -rf themes/PaperMod
-rm -rf .git/modules/themes/PaperMod
+rm -rf themes/PaperModPlus
+rm -rf .git/modules/themes/PaperModPlus
 
 # Remove the submodule entry from the index (if exists)
-git rm --cached -f themes/PaperMod
+git rm --cached -f themes/PaperModPlus
 
 echo "Adding PaperMod themes to the themes/ directory"
-git submodule add --depth=1 https://github.com/iamyaash/PaperModPlus.git themes/PaperModPlus
+# git submodule add --depth=1 https://github.com/iamyaash/PaperModPlus.git themes/PaperModPlus
+git submodule add --depth=1 https://codeberg.org/dovah-kiin/PaperModPlus.git themes/PaperModPlus
 
 # Sync submodules config (safety)
 git submodule sync --recursive
